786541003aec3e0e8dd0fd138a13e6a5707685a1
Tanja <tanja120a@gmail.com>
2020-12-25 20:59:54 +0100

    4. action dreator and redux-thunk

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-//REDUX
-import { createStore } from 'redux';
+//REDUX SETUP
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import rootReducer from './reducers/index';
+import thunk from 'redux-thunk';
 
-
-const store = createStore(rootReducer,   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__() );
+const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose; 
+const store = createStore(rootReducer,   composeEnhancer(applyMiddleware(thunk)) );
 
 ReactDOM.render(
   <React.StrictMode>
--- src/reducers/gamesReducer.js
+++ src/reducers/gamesReducer.js
@@ -6,11 +6,17 @@ const initState = {
 
 const gamesReducer = (state = initState, action) => {
     switch(action.type){
-        case "FETCH_GAME":
+        case "FETCH_GAMES":
             return{...state}
          default: 
         return{...state}    
     }
+}
+//ACTION CREATOR
+const fetchGames = () => {
+    return {
+        type: "FETCH_GAMES",
+    }    
 }
 
 export default gamesReducer;
